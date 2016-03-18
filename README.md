b31a8e4ca387a24717d33e0b6f835b2d3c67968e
jgunter7 <jason_gunter@hotmail.com>
2016-03-18 14:18:15 -0400

    Controls Update

--- README.md
+++
@@ -1,3 +0,0 @@
-#COMP392- Uphill Madness
-
-COMP392- Uphill Madness for COMP392 - Advanced Graphics @ Centennial College
--- Scripts/config/screen.ts
+++
@@ -1,8 +0,0 @@
-module config {
-    export class Screen {
-        static WIDTH:number = window.innerWidth;
-        static HEIGHT:number = window.innerHeight;
-        static RATIO:number = window.innerWidth / window.innerHeight;
-    }
-    
-}
